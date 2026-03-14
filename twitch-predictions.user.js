e12f07f7813c61371e66341ebdfbc4725157544b
HimanM <67066047+HimanM@users.noreply.github.com>
2026-03-15 04:52:34 +0530

    fix(meta): add additional exclusions for improved script functionality

--- twitch-predictions.user.js
+++ twitch-predictions.user.js
@@ -11,6 +11,14 @@
 // @updateURL    https://raw.githubusercontent.com/HimanM/Twitch-Predictions-Bot/master/twitch-predictions.user.js
 // @license      MIT
 // @match        https://www.twitch.tv/*
+// @exclude      https://www.twitch.tv/drops*
+// @exclude      https://www.twitch.tv/directory*
+// @exclude      https://www.twitch.tv/downloads*
+// @exclude      https://www.twitch.tv/jobs*
+// @exclude      https://www.twitch.tv/settings*
+// @exclude      https://www.twitch.tv/subs*
+// @exclude      https://www.twitch.tv/turbo*
+// @exclude      https://www.twitch.tv/wallet*
 // @grant        none
 // @run-at       document-idle
 // @icon         https://i.ibb.co/gbWGc64j/UNDERDOG-TW-PRED.png
